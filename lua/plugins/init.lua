c3e490e24c8d2105e0ae3f6efcb1e644413095e3
Zakhary Kaplan <me@zakhary.dev>
2022-08-18 21:10:00 -0400

    refactor: organize plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,37 +4,36 @@
 -- SPDX-License-Identifier: MIT
 -- Vim:         set fdl=0 fdm=marker:
 
--- This file can be loaded by calling `lua require("plugins")` from your init.lua
+-- This file can be loaded by calling `lua require("plugins")` in your init.lua
 -- Bootstrap {{{
--- Install packer if not currently installed
 local bootstrap
 do
-  local fn = vim.fn
+  -- Install packer if not currently installed
   local path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(path)) > 0 then
-    bootstrap = fn.system({
+  if vim.fn.empty(vim.fn.glob(path)) > 0 then
+    bootstrap = vim.fn.system {
       "git",
       "clone",
       "--depth=1",
       "https://github.com/wbthomason/packer.nvim",
       path
-    })
+    }
     vim.cmd [[packadd packer.nvim]]
   end
-end
 
--- Automatically run `:PackerCompile` whenever this file is updated
-vim.cmd [[
-  augroup Plugins
-    autocmd!
-    autocmd BufWritePost lua/**/*.lua source <afile> | PackerCompile
-  augroup end
-]]
+  -- Automatically run `:PackerCompile` whenever this file is updated
+  vim.cmd [[
+    augroup Plugins
+      autocmd!
+      autocmd BufWritePost lua/**/*.lua source <afile> | PackerCompile
+    augroup end
+  ]]
+end
 -- }}}
 
 -- Packer startup
 return require("packer").startup(function(use)
-  -- Colours {{{
+  -- Colorscheme {{{
   do
     use "EdenEast/nightfox.nvim"
     use "rebelot/kanagawa.nvim"
@@ -43,16 +42,10 @@ return require("packer").startup(function(use)
   end
   -- }}}
 
-  -- Code {{{
+  -- Editing {{{
   do
-    -- Snippet engine
-    use {
-      "dcampos/nvim-snippy",
-      config = function()
-        require("plugins.snippy")
-      end,
-      requires = "honza/vim-snippets",
-    }
+    -- Edit sandwiched textobjects
+    use "machakann/vim-sandwich"
     -- Smart and powerful comments
     use {
       "numToStr/Comment.nvim",
@@ -64,189 +57,6 @@ return require("packer").startup(function(use)
         }
       end
     }
-  end
-  -- }}}
-
-  -- Completion {{{
-  do
-    -- Auto completion plugin
-    use {
-      "hrsh7th/nvim-cmp",
-      config = function()
-        require("plugins.cmp")
-      end,
-      requires = {
-        -- Vim-builtin
-        "f3fora/cmp-spell",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-calc",
-        "hrsh7th/cmp-omni",
-        -- Neovim-builtin
-        "hrsh7th/cmp-nvim-lua",
-        "ray-x/cmp-treesitter",
-        -- Snippets
-        "dcampos/cmp-snippy",
-        -- Language Server Protocol
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lsp-document-symbol",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-        -- Filesystem
-        "hrsh7th/cmp-path",
-        "lukas-reineke/cmp-rg",
-        -- Extensions
-        "hrsh7th/cmp-cmdline",
-      },
-    }
-  end
-  -- }}}
-
-  -- Extensions {{{
-  do
-    -- Enhanced increment/decrement
-    use {
-      "monaqa/dial.nvim",
-      config = function()
-        require("plugins.dial")
-      end,
-    }
-    -- Modal multiple cursors
-    use {
-      "mg979/vim-visual-multi",
-      config = function()
-        vim.g.VM_leader = "\\"
-      end,
-    }
-    -- Searches, substitutions, and abbreviations
-    use "tpope/vim-abolish"
-    -- Unix file manipulation
-    use "tpope/vim-eunuch"
-    -- Repeat plugin maps
-    use "tpope/vim-repeat"
-    -- Heuristically set buffer options
-    use "tpope/vim-sleuth"
-    -- Handy bracket mappings
-    use "tpope/vim-unimpaired"
-    -- Additional text objects
-    use "wellle/targets.vim"
-    -- Handle line numbers in file names
-    use "wsdjeg/vim-fetch"
-  end
-  -- }}}
-
-  -- Filetype {{{
-  use {
-    { "rust-lang/rust.vim" },
-    {
-      "simrat39/rust-tools.nvim",
-      config = function()
-        require("rust-tools").setup {}
-      end,
-      requires = {
-        "mfussenegger/nvim-dap",
-        "nvim-lua/plenary.nvim",
-      },
-    },
-    ft = "rust",
-  }
-  -- }}}
-
-  -- Git {{{
-  do
-    -- Git integration for buffers
-    use {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("plugins.gitsigns")
-      end,
-      requires = "nvim-lua/plenary.nvim",
-    }
-    -- Magit for Neovim
-    use {
-      "TimUntersberger/neogit",
-      config = function()
-        require("neogit").setup {
-          integrations = {
-            diffview = true,
-          },
-        }
-      end,
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
-      },
-    }
-  end
-  -- }}}
-
-  -- LSP {{{
-  do
-    -- A pretty list for showing diagnostics
-    use {
-      "folke/trouble.nvim",
-      config = function()
-        local trouble = require("trouble")
-        -- Call setup
-        trouble.setup {}
-        -- Toggle trouble
-        vim.keymap.set("n", "<Space>d", trouble.toggle, {
-          noremap = true,
-          silent = true
-        })
-      end,
-      requires = "kyazdani42/nvim-web-devicons",
-    }
-    -- Ui for nvim-lsp progress
-    use {
-      "j-hui/fidget.nvim",
-      config = function()
-        require("fidget").setup {
-          text = {
-            -- Character shown when all tasks are completed
-            done = "✓",
-          }
-        }
-      end,
-    }
-    -- Use Neovim as a language server
-    use {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require("plugins.null-ls")
-      end,
-      requires = "nvim-lua/plenary.nvim",
-    }
-    -- Lsp common configurations
-    use {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require("plugins.lspconfig")
-      end,
-    }
-    -- Add pictograms to LSP
-    use "onsails/lspkind-nvim"
-  end
-  -- }}}
-
-  -- Packages {{{
-  do
-    -- Neovim plugin manager
-    -- NOTE: Packer can manage itself
-    use "wbthomason/packer.nvim"
-    -- Portable package manager for Neovim
-    use {
-      "williamboman/mason.nvim",
-      config = function()
-        require("plugins.mason")
-      end,
-      requires = "williamboman/mason-lspconfig.nvim",
-    }
-  end
-  -- }}}
-
-  -- Text {{{
-  do
-    -- Edit sandwiched textobjects
-    use "machakann/vim-sandwich"
     -- Super powerful autopairs
     use {
       "windwp/nvim-autopairs",
@@ -275,22 +85,221 @@ return require("packer").startup(function(use)
   end
   -- }}}
 
-  -- Treesitter {{{
+  -- Extensions {{{
   do
-    -- Treesitter configurations
+    -- Neovim {{{
+    do
+      -- Enhanced increment/decrement
+      use {
+        "monaqa/dial.nvim",
+        config = function()
+          require("plugins.dial")
+        end,
+      }
+      -- Modal multiple cursors
+      use {
+        "mg979/vim-visual-multi",
+        config = function()
+          vim.g.VM_leader = "\\"
+        end,
+      }
+      -- Searches, substitutions, and abbreviations
+      use "tpope/vim-abolish"
+      -- Unix file manipulation
+      use "tpope/vim-eunuch"
+      -- Repeat plugin maps
+      use "tpope/vim-repeat"
+      -- Heuristically set buffer options
+      use "tpope/vim-sleuth"
+      -- Handy bracket mappings
+      use "tpope/vim-unimpaired"
+      -- Additional text objects
+      use "wellle/targets.vim"
+      -- Handle line numbers in file names
+      use "wsdjeg/vim-fetch"
+    end
+    -- }}}
+
+    -- Git {{{
+    do
+      -- Git integration for buffers
+      use {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+          require("plugins.gitsigns")
+        end,
+        requires = "nvim-lua/plenary.nvim",
+      }
+      -- Magit for Neovim
+      use {
+        "TimUntersberger/neogit",
+        config = function()
+          require("neogit").setup {
+            integrations = {
+              diffview = true,
+            },
+          }
+        end,
+        requires = {
+          "nvim-lua/plenary.nvim",
+          "sindrets/diffview.nvim",
+        },
+      }
+    end
+    -- }}}
+  end
+  -- }}}
+
+  -- Intelligence {{{
+  do
+    -- Completion {{{
+    do
+      -- Auto completion plugin
+      use {
+        "hrsh7th/nvim-cmp",
+        config = function()
+          require("plugins.cmp")
+        end,
+        requires = {
+          -- Vim-builtin
+          "f3fora/cmp-spell",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-calc",
+          "hrsh7th/cmp-omni",
+          -- Neovim-builtin
+          "hrsh7th/cmp-nvim-lua",
+          "ray-x/cmp-treesitter",
+          -- Snippets
+          "dcampos/cmp-snippy",
+          -- Language Server Protocol
+          "hrsh7th/cmp-nvim-lsp",
+          "hrsh7th/cmp-nvim-lsp-document-symbol",
+          "hrsh7th/cmp-nvim-lsp-signature-help",
+          -- Filesystem
+          "hrsh7th/cmp-path",
+          "lukas-reineke/cmp-rg",
+          -- Extensions
+          "hrsh7th/cmp-cmdline",
+        },
+      }
+    end
+    -- }}}
+
+    -- Language {{{
+    do
+      use {
+        { "rust-lang/rust.vim" },
+        {
+          "simrat39/rust-tools.nvim",
+          config = function()
+            require("rust-tools").setup {}
+          end,
+          requires = {
+            "mfussenegger/nvim-dap",
+            "nvim-lua/plenary.nvim",
+          },
+        },
+        ft = "rust",
+      }
+    end
+    -- }}}
+
+    -- LSP {{{
+    do
+      -- A pretty list for showing diagnostics
+      use {
+        "folke/trouble.nvim",
+        config = function()
+          local trouble = require("trouble")
+          -- Call setup
+          trouble.setup {}
+          -- Toggle trouble
+          vim.keymap.set("n", "<Space>d", trouble.toggle, {
+            noremap = true,
+            silent = true
+          })
+        end,
+        requires = "kyazdani42/nvim-web-devicons",
+      }
+      -- Ui for nvim-lsp progress
+      use {
+        "j-hui/fidget.nvim",
+        config = function()
+          require("fidget").setup {
+            text = {
+              -- Character shown when all tasks are completed
+              done = "✓",
+            }
+          }
+        end,
+      }
+      -- Use Neovim as a language server
+      use {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require("plugins.null-ls")
+        end,
+        requires = "nvim-lua/plenary.nvim",
+      }
+      -- Lsp common configurations
+      use {
+        "neovim/nvim-lspconfig",
+        config = function()
+          require("plugins.lspconfig")
+        end,
+      }
+      -- Add pictograms to LSP
+      use "onsails/lspkind-nvim"
+    end
+    -- }}}
+
+    -- Snippets {{{
+    do
+      -- Snippet engine
+      use {
+        "dcampos/nvim-snippy",
+        config = function()
+          require("plugins.snippy")
+        end,
+        requires = "honza/vim-snippets",
+      }
+    end
+    -- }}}
+
+    -- Treesitter {{{
+    do
+      -- Treesitter configurations
+      use {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+          require("plugins.nvim-treesitter")
+        end,
+        run = ":TSUpdate",
+      }
+      -- Show code context
+      use "nvim-treesitter/nvim-treesitter-context"
+      -- Syntax aware text-objects
+      use "nvim-treesitter/nvim-treesitter-textobjects"
+      -- Auto-close and auto-rename markup tags
+      use "windwp/nvim-ts-autotag"
+    end
+    -- }}}
+  end
+  -- }}}
+
+  -- Packages {{{
+  do
+    -- Neovim plugin manager
+    -- NOTE: Packer can manage itself
+    use "wbthomason/packer.nvim"
+    -- Portable package manager for Neovim
     use {
-      "nvim-treesitter/nvim-treesitter",
+      "williamboman/mason.nvim",
       config = function()
-        require("plugins.nvim-treesitter")
+        require("plugins.mason")
       end,
-      run = ":TSUpdate",
+      requires = "williamboman/mason-lspconfig.nvim",
     }
-    -- Show code context
-    use "nvim-treesitter/nvim-treesitter-context"
-    -- Syntax aware text-objects
-    use "nvim-treesitter/nvim-treesitter-textobjects"
-    -- Auto-close and auto-rename markup tags
-    use "windwp/nvim-ts-autotag"
   end
   -- }}}
 
